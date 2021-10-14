6b551a7c21b2ae2d99ecce0a5b53143b40f55d33
Atishay Jain <66766830+atishay2411@users.noreply.github.com>
2021-10-14 21:01:10 +0530

    Update SymmetricDifference.py

--- Task-2/SymmetricDifference.py
+++ Task-2/SymmetricDifference.py
@@ -1,11 +1,8 @@
-M=int(input())
-list1=map(int,input().strip().split(" "))
-N=int(input())
-list2=map(int,input().strip().split(" "))
-set1=set(list1)
-set2=set(list2)
-l1=(set1.difference(set2))
-l2=(set2.difference(set1))
-l3=sorted(l1.union(l2))
-for i in l3:
-    print(i)
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+s1=set(input().split(" "))
+b=int(input())
+s2=set(input().split(" "))
+
+s3=s1.symmetric_difference(s2)
+print(len(s3))
