aa0afb82da581635a3be0e4a3de4c4cf5a120a60
Atishay Jain <66766830+atishay2411@users.noreply.github.com>
2021-10-14 21:02:20 +0530

    Update captainsRoom.py

--- Task-2/captainsRoom.py
+++ Task-2/captainsRoom.py
@@ -1,7 +1,11 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-k=int(input())
-a=set(input().split(" "))
-k=int(input())
-a=set(input().split(" "))
-"))
-print(a)
+n=int(input())
+nums=map(int,input().split(" "))
+nums=sorted(nums)
+for i in range(1,len(nums)):
+    if i!=len(nums)-1:
+        if nums[i]!=nums[i-1] and nums[i]!=nums[i+1]:
+            print(nums[i])
+            break
+    else:
+        print(nums[i])
